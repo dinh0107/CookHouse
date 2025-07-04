d3e98f30b471e77167f30421580dc8e2dcf0d3fa
Anh Quang <anhquang.tran@vico.vn>
2025-07-04 12:49:40 +0700

    update all

---
+++ CookHouse/Migrations/202506100934391_AddBlockedIpLogs.cs
@@ -0,0 +1,29 @@
+﻿namespace CookHouse.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBlockedIpLogs : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BlockedIpLogs",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        IP = c.String(maxLength: 50),
+                        BlockedAt = c.DateTime(nullable: false),
+                        RequestCount = c.Int(nullable: false),
+                        IsActive = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.BlockedIpLogs");
+        }
+    }
+}
